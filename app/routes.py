15418c85c902ea7c4d58316a175bbaea320d5605
Nuwe Bryant Nyero <nuwebryant@gmail.com>
2024-12-28 14:01:38 +0300

    Include template inheritance between index.html and base.html

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def func():
-    return render_template('index.html', title='Ecommerce')
+    user = {'username': 'Larru'}
+    return render_template('index.html', title='Ecommerce', user=user)
